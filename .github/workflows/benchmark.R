3022dad8bf61ef5f47b793f20f5b87aed9948e86
Bjarke Hautop <bjarke.kristensen@sund.ku.dk>
2026-03-26 14:16:22 +0100

    Format benchmark.R

--- .github/workflows/benchmark.R
+++ .github/workflows/benchmark.R
@@ -2,12 +2,20 @@ library(optparse)
 library(caugi)
 
 option_list <- list(
-  make_option(c("--output"), type="character", default="pr_time.rds",
-              help="RDS file to save benchmark results"),
-  make_option(c("--branch-name"), type="character", default="PR branch",
-              help="Name of branch for messages")
+  make_option(
+    c("--output"),
+    type = "character",
+    default = "pr_time.rds",
+    help = "RDS file to save benchmark results"
+  ),
+  make_option(
+    c("--branch-name"),
+    type = "character",
+    default = "PR branch",
+    help = "Name of branch for messages"
+  )
 )
-opt <- parse_args(OptionParser(option_list=option_list))
+opt <- parse_args(OptionParser(option_list = option_list))
 
 set.seed(1405)
 n <- 1000
@@ -15,7 +23,7 @@ p <- 0.25
 
 cat(sprintf("Running benchmark on %s...\n", opt$branch_name))
 
-cg <- generate_graph(n, p, class="DAG")
+cg <- generate_graph(n, p, class = "DAG")
 build(cg)
 
 test_node_index <- sample(n, 1)
@@ -30,11 +38,20 @@ bench_result <- bench::mark(
   descendants = descendants(cg, test_node_name),
   subgraph = subgraph(cg, test_node_name),
   d_seperated = {
-    valid_adjustment_set <- adjustment_set(cg, test_node_name, test_node_name_new, type="backdoor")
+    valid_adjustment_set <- adjustment_set(
+      cg,
+      test_node_name,
+      test_node_name_new,
+      type = "backdoor"
+    )
     d_separated(cg, test_node_name, test_node_name_new, valid_adjustment_set)
   },
   check = FALSE
 )
 
 saveRDS(bench_result, opt$output)
-cat(sprintf("%s benchmark done, results saved to %s\n", opt$branch_name, opt$output))
+cat(sprintf(
+  "%s benchmark done, results saved to %s\n",
+  opt$branch_name,
+  opt$output
+))
